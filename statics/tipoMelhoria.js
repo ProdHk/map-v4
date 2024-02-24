616d4d261a60c8bebbd733c8a3567e9e0c20bb62
Ivan Jr <junioh2001@gmail.com>
2024-02-24 09:57:11 -0300

    add static tipoMelhoria

---
+++ statics/tipoMelhoria.js
@@ -0,0 +1,42 @@
+export const tipoMelhoria = [
+    {
+        id: 0,
+        name: "Sugestão",
+        value: "sugestao"
+    },
+    {
+        id: 1,
+        name: "Erro interno",
+        value: "erro-interno"
+    },
+    {
+        id: 2,
+        name: "Problemas com atendimento",
+        value: "problemas-atendimento"
+    },
+    {
+        id: 3,
+        name: "Erro de sistema",
+        value: "erro-sistema"
+    },
+    {
+        id: 4,
+        name: "Telefonia",
+        value: "telefonia"
+    },
+    {
+        id: 5,
+        name: "Veículos da empresa",
+        value: "veiculos"
+    },
+    {
+        id: 6,
+        name: "Equipamentos",
+        value: "equipamentos"
+    },
+    {
+        id: 7,
+        name: "Infraestrutura",
+        value: "infraestrutura"
+    },
+]
